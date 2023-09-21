ce2dd1d0c3a2e3ab59674ab483678b0f6db844ee
Elite_Book <abdelmounaim,abounore@gmail.com>
2023-09-21 02:50:58 +0100

    Win when 5 horizontal symbols are completed

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+let squares = new Array(400);
+
+const board = document.querySelector(".board");
+
+for(let i = 0; i < 400; i++){
+
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.id = `${i}`;
+
+    square.onclick = function() {
+        game(this.id);
+    };
+
+    board.appendChild(square);
+}
+
+// const winner = (id) => {
+//     let squares = new Array(400);
+//     var element = document.getElementById(`square-${id}`);
+//     console.log(element);
+// }
+
 let character = 'X';
 let title = document.querySelector('.title');
 
@@ -13,19 +36,20 @@ const game = (id) => {
         character = 'X';
         title.innerHTML = "The turn of X";
     }
-}
 
-const board = document.querySelector(".board");
-
-for(let i = 0; i < 400; i++){
-
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.id = `square-${i}`;
-
-    square.onclick = function() {
-        game(this.id);
-    };
-
-    board.appendChild(square);
+    squares[id] = element.innerHTML;
+    for(let i = 0; i < squares.length; i++){
+        if(squares[i] == squares[i+1] 
+            && squares[i+1] == squares[i+2] 
+            && squares[i+2] == squares[i+3] 
+            && squares[i+3] == squares[i+4]
+            && squares[i] != undefined) {
+            
+            for(let j = i; j < i + 5; j++){
+                let winnerSquare = document.getElementById(j); 
+                winnerSquare.style.backgroundColor = "green";
+                winnerSquare.style.color = "white";
+            }
+        }
+    }
 }
